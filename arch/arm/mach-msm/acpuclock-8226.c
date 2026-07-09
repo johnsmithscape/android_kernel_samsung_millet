12feea9e7c8b798274ce527bbc5ecc780ef29314
Johnsmithscape <vankinok@hotmail.com>
2026-07-09 15:31:13 +0300

    Stock kernel 5.1.1

--- arch/arm/mach-msm/acpuclock-8226.c
+++ arch/arm/mach-msm/acpuclock-8226.c
@@ -60,9 +60,6 @@ static struct msm_bus_scale_pdata bus_client_pdata = {
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p1[] = {
-	{ 1,  60000, PLL0,    4, 2,   CPR_CORNER_1,    0, 4 },
-	{ 1,  192000, ACPUPLL,    5, 2,   CPR_CORNER_1,    0, 4 },
-	{ 1,  249600, ACPUPLL,    5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 4 },
@@ -74,9 +71,6 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p1[] = {
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p2[] = {
-	{ 1,  60000, PLL0,    4, 2,   CPR_CORNER_1,    0, 4 },
-	{ 1,  192000, ACPUPLL,    5, 2,   CPR_CORNER_1,    0, 4 },
-	{ 1,  249600, ACPUPLL,    5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 4 },
@@ -88,9 +82,6 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p2[] = {
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p4[] = {
-	{ 1,  60000, PLL0,    4, 2,   CPR_CORNER_1,    0, 4 },
-	{ 1,  192000, ACPUPLL,    5, 2,   CPR_CORNER_1,    0, 4 },
-	{ 1,  249600, ACPUPLL,    5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 4 },
@@ -105,9 +96,6 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p4[] = {
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p6[] = {
-	{ 1,  60000, PLL0,    4, 2,   CPR_CORNER_1,    0, 4 },
-	{ 1,  192000, ACPUPLL,    5, 2,   CPR_CORNER_1,    0, 4 },
-	{ 1,  249600, ACPUPLL,    5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 4 },
@@ -124,9 +112,6 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p6[] = {
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8610[] = {
-	{ 1,  60000, PLL0,    4, 2,   CPR_CORNER_1,    0, 4 },
-	{ 1,  192000, ACPUPLL,    5, 2,   CPR_CORNER_1,    0, 4 },
-	{ 1,  249600, ACPUPLL,    5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 3 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 3 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 4 },
@@ -163,8 +148,8 @@ static struct acpuclk_drv_data drv_data = {
 		.update_mask = RCG_CONFIG_UPDATE_BIT,
 		.poll_mask = RCG_CONFIG_UPDATE_BIT,
 	},
-	.power_collapse_khz = 192000,
-	.wait_for_irq_khz = 192000,
+	.power_collapse_khz = 300000,
+	.wait_for_irq_khz = 300000,
 };
 
 static int __init acpuclk_a7_probe(struct platform_device *pdev)
